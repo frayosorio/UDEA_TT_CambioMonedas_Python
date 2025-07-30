64fb8276cbb33d7d5cf682b1c86d5dd02332d3ff
fray osorio <frayosorio@gmail.com>
2025-07-30 17:26:26 -0500

    Se crea la interfaz grafica de usuario

---
+++ CambiosMonedas.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import Util
+
+v = Tk()
+v.title("Cambios de Moneda")
+v.geometry("400x300")
+
+iconos =["./iconos/Grafica.png", "./iconos/Datos.png"]
+textos=["Gráfica Cambio vs Fecha", "Estadísticas"]
+
+botones = Util.agregarBarra(v, iconos, textos)
+
+#Agregar panel para selecionar monedas y rango de fechas
+panel = Frame(v)
+panel.pack(side=TOP, fill=X)
+
+Util.agregarEtiqueta(panel, "Moneda:", 0, 0)
+cmbMoneda = Util.agregarLista(panel, [], 0, 1)
+Util.agregarEtiqueta(panel, "Desde:", 0, 2)
+cldDesde = Util.agregarCalendario(panel, 0, 3)
+Util.agregarEtiqueta(panel, "Hasta:", 0, 4)
+cldHasta = Util.agregarCalendario(panel, 0, 5)
+
+
+
+
+
